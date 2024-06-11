5ed04e72f940679705dd39c28e2778ac955fa0ec
LaurenceJorissen <99961435+LaurenceJorissen@users.noreply.github.com>
2024-06-11 12:06:54 +0200

    nu gaat het

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,4 +1,2 @@
 import { createClient } from '@supabase/supabase-js';
-PUBLIC_SUPABASE_URL = "https://liaxjaygctgiiqbzotew.supabase.co";
-PUBLIC_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxpYXhqYXlnY3RnaWlxYnpvdGV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcwNzE1MTEsImV4cCI6MjAzMjY0NzUxMX0.p3LU_2BGaNVNC7GtG_NWsVQp3lkMKqJDJZqydnFpjm0";
-export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
+export const supabase = createClient('https://liaxjaygctgiiqbzotew.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxpYXhqYXlnY3RnaWlxYnpvdGV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcwNzE1MTEsImV4cCI6MjAzMjY0NzUxMX0.p3LU_2BGaNVNC7GtG_NWsVQp3lkMKqJDJZqydnFpjm0');
